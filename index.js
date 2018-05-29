b4639fd8104770c1bb8bbe6759c1bbf706d08fa9
Mohammed Rashid <muhammad.rashid@avanzainnovations.com>
2018-05-29 11:29:15 +0400

    Updated

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ var options = {};
 soap.createClient(url, options, function(err, client) {
   var method = client["ListOfContinentsByName"];
   method(requestArgs, function(err, result, envelope, soapHeader) {
-    //response envelope
-    console.log("Response Envelope: \n" + envelope);
-    //'result' is the response body
     console.log("Result: \n" + JSON.stringify(result));
   });
 });
