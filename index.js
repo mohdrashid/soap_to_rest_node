fa00e5004466ef78d1f7d52bb844dd6c37a91c29
Mohammed Rashid <muhammad.rashid@avanzainnovations.com>
2018-05-29 12:09:00 +0400

    Added express

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
 "use strict";
+const soap = require("strong-soap").soap;
+const config = require('./config.json')
+const express = require('express')
+const app = express();
+const port = config.port;
+const url = config.wsdlURL;
+const options = {};
 
-var soap = require("strong-soap").soap;
-var url = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL";
+const bodyParser = require('body-parser')
+let soapClient;
 
-var requestArgs = {
-};
-
-var options = {};
 soap.createClient(url, options, function(err, client) {
-  var method = client["ListOfContinentsByName"];
-  method(requestArgs, function(err, result, envelope, soapHeader) {
-    console.log("Result: \n" + JSON.stringify(result));
-  });
+  soapClient = client;
 });
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+/* Sample Body
+{
+
+}
+*/
+
+app.post('/', (req, res) => {
+  const method = soapClient[req.body.function];
+  method(req.body.args, function(err, result, envelope, soapHeader) {
+    res.json(result)
+  });
+})
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+  console.log(`server is listening on ${port}`)
+})
