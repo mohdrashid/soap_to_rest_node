9982f43349b03c2c2fda0016f4607ca65c42b158
Mohammed Rashid <muhammad.rashid@avanzainnovations.com>
2018-05-29 11:22:03 +0400

    Working example

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 "use strict";
 
-var soap = require('strong-soap').soap;
-// wsdl of the web service this client is going to invoke. For local wsdl you can use, url = './wsdls/stockquote.wsdl'
-var url = 'http://www.webservicex.net/stockquote.asmx?WSDL';
+var soap = require("strong-soap").soap;
+var url = "http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL";
 
 var requestArgs = {
-  symbol: 'IBM'
 };
 
 var options = {};
 soap.createClient(url, options, function(err, client) {
-    console.log(err)
-  var method = client['StockQuote']['StockQuoteSoap']['GetQuote'];
+  var method = client["ListOfContinentsByName"];
   method(requestArgs, function(err, result, envelope, soapHeader) {
     //response envelope
-    console.log('Response Envelope: \n' + envelope);
+    console.log("Response Envelope: \n" + envelope);
     //'result' is the response body
-    console.log('Result: \n' + JSON.stringify(result));
+    console.log("Result: \n" + JSON.stringify(result));
   });
 });
